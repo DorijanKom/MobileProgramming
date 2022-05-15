f84c26f562b412f6a023ecce85fd096f9a1c6621
DorijanKom <dorijan.komsic@stu.ibu.edu.ba>
2022-05-15 23:05:19 +0200

    Started implementing fragments

---
+++ app/src/main/java/com/example/lively/DashboardFragment.java
@@ -0,0 +1,4 @@
+package com.example.lively;
+
+public class DashboardFragment {
+}
---
+++ app/src/main/java/com/example/lively/ViewPageAdapter.java
@@ -0,0 +1,31 @@
+package com.example.lively;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewPageAdapter extends FragmentStateAdapter {
+
+    private final List<Fragment> fragmentList = new ArrayList<>();
+    public ViewPageAdapter(@NonNull Fragment fragment) {
+        super(fragment);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return fragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment){
+        fragmentList.add(fragment);
+    }
+}
