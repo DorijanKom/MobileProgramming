ac45cd2328c90605cdd0e2d889a33c4132076784
KenanLokvancic <kenan.lokvancic@stu.ibu.edu.ba>
2022-06-11 19:30:23 +0200

    added Spinner and user data fragment

--- app/src/main/java/com/example/lively/fragments/exercise/ExerciseFragment.java
+++ app/src/main/java/com/example/lively/fragments/exercise/ExerciseFragment.java
@@ -11,18 +11,56 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.lively.R;
+import com.example.lively.fragments.home.HomeFragment;
+import com.example.lively.fragments.schedule.ScheduleFragment;
 
 public class ExerciseFragment extends Fragment {
-
 
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_exercise,container,false);
+
+        View view = inflater.inflate(R.layout.fragment_exercise, container, false);
+
+        Button set_schedule = view.findViewById(R.id.btnSchedule);
+
+
+        set_schedule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                getParentFragmentManager().beginTransaction().replace(R.id.frag_container_view, new ScheduleFragment()).commit();
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        return view;
+
 
 
     }
 
 
+
 }
+
+
+
---
+++ app/src/main/java/com/example/lively/fragments/user_data/UserDataFragment.java
@@ -0,0 +1,4 @@
+package com.example.lively.fragments.user_data;
+
+public class UserDataFragment {
+}
