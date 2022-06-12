e76dfe4c93da18d7cee06b60178fa1943cd9a6e1
KenanLokvancic <kenan.lokvancic@stu.ibu.edu.ba>
2022-06-12 21:37:33 +0200

    Added schedule fragment

--- app/src/main/java/com/example/lively/fragments/exercise/ExerciseFragment.java
+++ app/src/main/java/com/example/lively/fragments/exercise/ExerciseFragment.java
@@ -5,16 +5,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.lively.MainActivity;
 import com.example.lively.R;
+import com.example.lively.database.LivelyDatabase;
+import com.example.lively.database.tables.Users;
 import com.example.lively.fragments.home.HomeFragment;
 import com.example.lively.fragments.schedule.ScheduleFragment;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class ExerciseFragment extends Fragment {
+    List<Users> usersList = new ArrayList<>();
+
+    EditText exerciseTime;
+    TextView nextExercise;
 
 
     @Nullable
@@ -24,16 +39,25 @@ public class ExerciseFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_exercise, container, false);
 
         Button set_schedule = view.findViewById(R.id.btnSchedule);
+        Button find_gym = view.findViewById(R.id.btnGym);
 
+        // TO be removed.
+        exerciseTime = view.findViewById(R.id.exercisedFor_text);
+        nextExercise = view.findViewById(R.id.nextExercise_text);
+
+        nextExercise.setText(new SimpleDateFormat("MM/dd/yyyy").format(new Date(((MainActivity) getActivity()).user.getNextExercise())));
 
         set_schedule.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+
                 getParentFragmentManager().beginTransaction().replace(R.id.frag_container_view, new ScheduleFragment()).commit();
 
             }
         });
+
+
 
 
 
--- app/src/main/java/com/example/lively/fragments/schedule/ScheduleFragment.java
+++ app/src/main/java/com/example/lively/fragments/schedule/ScheduleFragment.java
@@ -3,21 +3,59 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CalendarView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.lively.MainActivity;
 import com.example.lively.R;
+import com.example.lively.database.LivelyDatabase;
+import com.example.lively.database.tables.Users;
+import com.example.lively.fragments.exercise.ExerciseFragment;
+
+import java.util.Calendar;
 
 public class ScheduleFragment extends Fragment {
+
+    private CalendarView calendar;
+    private Button save, back;
+
+    private Users user;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
         View view = inflater.inflate(R.layout.fragment_set_schedule, container, false);
 
+        calendar = view.findViewById(R.id.calendarView);
+        save = view.findViewById(R.id.btnSave);
+        back = view.findViewById(R.id.btnBack);
+
+        user = ((MainActivity)getActivity()).user;
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                user.setNextExercise(calendar.getDate());
+                LivelyDatabase.getDatabase(getContext()).usersDAO().updateUsers(user);
+                navigate();
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navigate();
+            }
+        });
+
         return view;
+    }
+
+    private void navigate() {
+        getParentFragmentManager().beginTransaction().replace(R.id.frag_container_view, new ExerciseFragment()).commit();
     }
 }
